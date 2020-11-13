eeba5fc377de8d9b058277cdb4be9b4d3bd117c8
adelevrc <adele.vercaygne@gmail.com>
2020-11-13 10:56:16 +0100

    rajout indication construction app.js

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-var express = require("express");
-var app = express();
+// j'appelle express et mongoose et body parser ! 
+
+// je fais en sorte qu'express utilise sa méthode de router et je vais en sorte que app l'utilise ! 
 
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+// Je créé une variable qui va permettre d'executer express
 
 
-app.get("/lala", (req, res, next) => {
-    res.json(["Tony","Lisa","trouducul","Ginger","Food"]);
-});
+// je connecte ma database
+
+// j'évite les problèmes de CORS 
+
+// je crée des  middlewares qui vont faire en sorte qu'à chaque fois que j'attend un chemin, j'ai ma fonction qui s'execute (ne pas oublier body parser)
+
+// je créé mon modèle d'articles 
+
+/* je créé mes routes en fonction de trois chemins différents : 
+- un chemin pour accueil 
+- un chemin pour le formulaire
+- un chemin pour le détail en fonction de l'article demandé */ 
+
+
+
+
+// Je fais en sorte que mon fichier soit écouté sur le port 3000 
+
+
+// je fais en sorte que mes données soit exportées sur mongoose 
