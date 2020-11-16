868ea8fc6ee071bf11c05a913192760c63dc3c8e
manekineko17 <mgrouiller@gmail.com>
2020-11-16 12:04:33 +0100

    création routes

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 // j'appelle express et mongoose et body parser ! 
-
 const bodyParser = require('body-parser'); //import de body parser
 const mongoose = require('mongoose'); //import de mongoose
+const postRoutes = require('./routes/post');    //importation du router
 
-<<<<<<< HEAD
 // je fais en sorte qu'express utilise sa méthode de router et je vais en sorte que app l'utilise !
-
-/*Une application Express est une série de fonctions appelées middleware. Chaque élément de middleware
-*reçoit les objets request et response, peut les lire, les analyser et les manipuler. Le middleware Express
-*reçoit ausssi la méthode next qui permet à chaque middleware de passer l'exécution au middleware suivant.*/ 
-
 const express = require('express'); //import d'express + commande require
 
-=======
->>>>>>> 04133d115bd77c7ae47457621048622ad7d78f3d
 // Je créé une variable qui va permettre d'executer express
-
 const app = express(); //appel de la méthode express pour créer une appli express
 
 // je connecte ma database
-
 mongoose.connect('lien_data', /*modifier le lien*/
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -36,65 +26,26 @@ app.use((req, res, next) => {
 });
 
 // je crée des  middlewares qui vont faire en sorte qu'à chaque fois que j'attend un chemin, j'ai ma fonction qui s'execute (ne pas oublier body parser)
-
 app.use(bodyParser.json()); //middleware global. à partir de ce middleware, on a accès au corps de la req 
 
 // je créé mon modèle d'articles 
-
 const router = express.Router();
-const Article = require('./models/Article');
-
-router.post('/routes/post', (req, res, next) => {
-  delete req.body._id;
-  const article = new Article({
-  ...req.body
-  });
-  article.save()
-  .then(() => res.status(201).json({ message: 'Objet enregistré!'}))
-  .catch(error => res.status(400).json({ error }));
-});
-
-router.put('/routes/post/:id', (req, res, next) => {
-  Article.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-router.delete('/routes/post/:id', (req, res, next) => {
-  Article.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-router.get('/routes/post/:id', (req, res, next) => {
-  Article.findOne({ _id: req.params.id })
-    .then(article => res.status(200).json(article))
-    .catch(error => res.status(404).json({ error }));
-});
-
-router.get('/routes/post', (req, res, next) => {
-  Article.find()
-  .then(Articles => res.status(200).json(Articles))
-  .catch(error => res.status(400).json({ error }))
-});
 
 /* je créé mes routes en fonction de trois chemins différents : 
 - un chemin pour accueil 
 - un chemin pour le formulaire
 - un chemin pour le détail en fonction de l'article demandé */ 
 
-const postRoutes = require('./routes/post');
 app.use('/post', postRoutes); // middleware
-
-
+//si erreur, essayer avec app.use('/api/post', postRoutes); 
 
 // Je fais en sorte que mon fichier soit écouté sur le port 3000 
-
-
 // je fais en sorte que mes données soit exportées sur mongoose 
 
-
-
-
-
 module.exports = app;
+
+
+
+/*Une application Express est une série de fonctions appelées middleware. Chaque élément de middleware
+*reçoit les objets request et response, peut les lire, les analyser et les manipuler. Le middleware Express
+*reçoit ausssi la méthode next qui permet à chaque middleware de passer l'exécution au middleware suivant.*/ 
--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('../models/Article');
 
-router.get('/post', (req, res) => {
+router.get('/', (req, res) => {
     res.send("Hello, je viens du fichier post.js");
 })
+
+router.post('/', (req, res, next) => {
+    delete req.body._id;
+    const article = new Article({
+    ...req.body
+    });
+    article.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré!'}))
+    .catch(error => res.status(400).json({ error }));
+});
+  
+router.put('/:id', (req, res, next) => {
+    Article.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+  
+router.delete('/:id', (req, res, next) => {
+    Article.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+router.get('/:id', (req, res, next) => {
+    Article.findOne({ _id: req.params.id })
+    .then(article => res.status(200).json(article))
+    .catch(error => res.status(404).json({ error }));
+});
+
+router.get('/', (req, res, next) => {
+    Article.find()
+.then(Articles => res.status(200).json(Articles))
+.catch(error => res.status(400).json({ error }))
+});
 
 module.exports = router;
